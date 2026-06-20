cb32523777e1b268c533ec5438f69f227d502b55
Milena Jaimes <cony0628@hotmail.com>
2026-06-19 20:36:22 -0500

    se agregan casos negativos login
    Se implementan los casos negativos para el login username incorrecto , password incorrecto

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,39 @@
 import { expect, test } from '@playwright/test';
-test ('Login to hrm', async({page})=>{
+test.describe('Login Test', () => {
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin') 
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    })
+  test('Login to hrm', async ({ page }) => {
+    let username: string = 'Admin';
+    let password: string = 'admin123';
+    
+    await page.getByRole('textbox', { name: 'Username' }).fill(username) 
+    await page.getByRole('textbox', { name: 'Password' }).fill(password)
     await page.getByRole('button', { name: 'Login' }).click()
 
     await expect(page.getByRole('link',{ name: 'Admin' })).toBeVisible()
+})
+test('Login to hrm with invalid credentials- invalid username ', async ({ page }) => { 
+  let invalidUsername: string = 'AdminIncorrect';
+  let password: string = 'admin123';
+  
+  await page.getByRole('textbox', { name: 'Username' }).fill(invalidUsername) 
+  await page.getByRole('textbox', { name: 'Password' }).fill(password)
+  await page.getByRole('button', { name: 'Login' }).click() 
+  await expect(page.getByText('Invalid credentials')).toBeVisible()   
+    
+})
+
+test('Login with password incorrect', async ({ page }) => {
+  let username: string = 'Admin';
+  let invalidPassword: string = 'PasswordIncorrect';
+  
+  await page.getByRole('textbox', { name: 'Username' }).fill(username)
+  await page.getByRole('textbox', { name: 'Password' }).fill(invalidPassword)
+  await page.getByRole('button', { name: 'Login' }).click()
+
+  await expect(page.getByText('Invalid credentials')).toBeVisible()
+
+})
 })
